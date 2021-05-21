06f74f9101a0922537e0b43aba9ccc8083c97c32
Kurt Fuoco <fuocokurt@gmail.com>
2021-05-20 20:46:51 -0700

    remove oudated code from client.js

--- static/script/client.js
+++ static/script/client.js
@@ -26,28 +26,3 @@ $(document).ready(function() {
     });
 
 });
-
-// Does n
-$("#submidt").click(function () {
-    $.ajax({
-        url: "/authenticate",
-        type: "POST",
-        dataType: "JSON",
-        data: {username: $("#username").val() },
-        success: function (data) {
-            console.log("Data returned from server: ", data);
-            if (data['status'] == "success") {
-                // redirect
-                window.location.replace("/landing");
-            } else {
-                // show error message
-                $("#errorMsg").html(data['msg']);
-            }
-
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            $("body").text(jqXHR.statusText);
-        }
-    });
-
-});
